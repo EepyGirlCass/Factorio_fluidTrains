93fa2c19010236d74d8633eb8ed898fa8b52ef70
Cass <114196473+Nicxxk@users.noreply.github.com>
2025-06-06 23:18:04 -0400

    add support for half diagonals

--- stdlib/util.lua
+++ stdlib/util.lua
@@ -5,29 +5,45 @@ end
 local dir = defines.direction
 local directiontable = {
 	dir.north,
+	dir.northnortheast,
 	dir.northeast,
+	dir.eastnortheast,
 	dir.east,
+	dir.eastsoutheast,
 	dir.southeast,
+	dir.southsoutheast,
 	dir.south,
+	dir.southsouthwest,
 	dir.southwest,
+	dir.westsouthwest,
 	dir.west,
+	dir.westnorthwest,
 	dir.northwest,
-	dir.north
+	dir.northnorthwest
 }
+
+local half_diagonal_offset = math.tan(22.5 * math.pi / 180) -- 22.5 degrees in radians
 
 int_to_vector = {
 	{x = 0, y = -1},
+	{x = half_diagonal_offset, y = -1},
 	{x = 1, y = -1},
+	{x = 1, y = -half_diagonal_offset},
 	{x = 1, y = 0},
+	{x = 1, y = half_diagonal_offset},
 	{x = 1, y = 1},
+	{x = half_diagonal_offset, y = 1},
 	{x = 0, y = 1},
+	{x = -half_diagonal_offset, y = 1},
 	{x = -1, y = 1},
-	{x = -1, y = 1},
+	{x = -1, y = half_diagonal_offset},
 	{x = -1, y = 0},
-	{x = -1, y = -1}
+	{x = -1, y = -half_diagonal_offset},
+	{x = -1, y = -1},
+	{x = -half_diagonal_offset, y = -1}
 }
 ori_to_dir = function(ori)
-	return math.floor(8*ori)
+	return math.floor(16*ori)
 end
 
 dir_to_int = function(direction)
